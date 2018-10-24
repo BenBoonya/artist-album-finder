fa7a188508620bdaff61fc1925bc20d2b40eaaa5
BenBoonya <benbenn223@gmail.com>
2018-10-24 21:06:04 +0700

    provide data list

--- app/src/main/java/com/skooldio/android/artistalbumfinder/MainActivity.kt
+++ app/src/main/java/com/skooldio/android/artistalbumfinder/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.skooldio.android.artistalbumfinder
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.skooldio.android.artistalbumfinder.model.Artist
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val artistList: List<Artist> = listOf(
+            Artist("Twenty One Pilot", "Alternative"),
+            Artist("Bodyslam", "Rock"),
+            Artist("Retrospect", "Metal/Emo"),
+            Artist("BNK48"),
+            Artist("Oasis", "British Rock")
+        )
     }
 }
