eb11cce813ef63066f54de135f7725ecf630c139
BenBoonya <benbenn223@gmail.com>
2018-10-25 22:59:24 +0700

    show data on rv

--- app/src/main/java/com/skooldio/android/artistalbumfinder/MainActivity.kt
+++ app/src/main/java/com/skooldio/android/artistalbumfinder/MainActivity.kt
@@ -2,7 +2,10 @@ package com.skooldio.android.artistalbumfinder
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import com.skooldio.android.artistalbumfinder.adapter.ArtistAdapter
 import com.skooldio.android.artistalbumfinder.model.Artist
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,11 +14,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val artistList: List<Artist> = listOf(
-            Artist("Twenty One Pilot", "Alternative"),
-            Artist("Bodyslam", "Rock"),
-            Artist("Retrospect", "Metal/Emo"),
-            Artist("BNK48"),
-            Artist("Oasis", "British Rock")
+                Artist("Twenty One Pilot", "Alternative"),
+                Artist("Bodyslam", "Rock"),
+                Artist("Retrospect", "Metal/Emo"),
+                Artist("BNK48"),
+                Artist("Oasis", "British Rock")
         )
+        val adapter = ArtistAdapter()
+
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = adapter
+
+        adapter.values = artistList
+        adapter.notifyDataSetChanged()
     }
 }
