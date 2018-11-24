1c844be1e216461e07e954cabdb593c726aacdfa
BenBoonya <benbenn223@gmail.com>
2018-11-24 18:03:06 +0700

    call artist api

--- app/src/main/java/com/skooldio/android/artistalbumfinder/MainActivity.kt
+++ app/src/main/java/com/skooldio/android/artistalbumfinder/MainActivity.kt
@@ -5,8 +5,14 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.skooldio.android.artistalbumfinder.adapter.ArtistAdapter
+import com.skooldio.android.artistalbumfinder.api.Api
 import com.skooldio.android.artistalbumfinder.model.Artist
+import com.skooldio.android.artistalbumfinder.model.ArtistApi
+import com.skooldio.android.artistalbumfinder.model.ResponseWrapper
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,12 +40,30 @@ class MainActivity : AppCompatActivity() {
             this.adapter = adapter
         }
 
-        adapter.values = artistList
+        getArtistList("body")
+    }
+
+    private fun getArtistList(searchKey: String) {
+        Api.api.searchForArtist(searchKey).enqueue(object : Callback<ResponseWrapper<ArtistApi>> {
+            override fun onFailure(call: Call<ResponseWrapper<ArtistApi>>, t: Throwable) {
+            }
+
+            override fun onResponse(
+                call: Call<ResponseWrapper<ArtistApi>>,
+                response: Response<ResponseWrapper<ArtistApi>>
+            ) {
+                if (response.isSuccessful) {
+                    (recyclerView.adapter as ArtistAdapter).values =
+                            response.body()?.result?.map { Artist(it.name, it.genreName, it.artistId.toInt()) } ?:
+                            ArrayList()
+                }
+            }
+        })
     }
 
     private fun navigateToAlbumList(artist: Artist) {
-        startActivity(Intent(this, AlbumListActivity::class.java).apply {
-            putExtra(ARTIST_NAME_EXTRA, artist.name)
-        })
+        val intent = Intent(this, AlbumListActivity::class.java)
+        intent.putExtra(ARTIST_NAME_EXTRA, artist.name)
+        startActivity(intent)
     }
 }
