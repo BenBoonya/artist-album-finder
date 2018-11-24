b8ef0176228c8a8b37bc78159ebcacdf309dfa28
BenBoonya <benbenn223@gmail.com>
2018-11-24 17:37:21 +0700

    fix model and manifest

--- app/src/main/java/com/skooldio/android/artistalbumfinder/model/ArtistApi.kt
+++ app/src/main/java/com/skooldio/android/artistalbumfinder/model/ArtistApi.kt
@@ -6,7 +6,7 @@ import com.google.gson.annotations.SerializedName
  * Created by Boonya Kitpitak on 10/28/18.
  */
 data class ArtistApi(@SerializedName("artistType") val type: String,
-                     @SerializedName("artistType") val name: String,
+                     @SerializedName("artistName") val name: String,
                      @SerializedName("artistLinkUrl") val linkUrl: String,
                      @SerializedName("artistId") val artistId: String,
                      @SerializedName("primaryGenreName") val genreName: String,
